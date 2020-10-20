f81eb3d9894f2e2963396dcdd8e165bf312e9eb6
DESKTOP-6GUHKBS\SirLight <sirlight.k@gmail.com>
2020-10-20 23:03:11 +0700

    restructure charities requests

--- src/requests/charities_requests.ts
+++ src/requests/charities_requests.ts
@@ -3,15 +3,15 @@ import { isGetCharitiesResponse } from "../response/charities_response";
 import { BaseAPIConfig, ContentType, HttpMethod } from "./base_http_request";
 import { GenericHTTPRequest } from "./generic_http_request";
 
-const URL = "/charities";
+const charitiesConfig: BaseAPIConfig = {
+  method: undefined,
+  contentType: ContentType.JSON,
+  url: "/charities",
+};
 
 export class GetCharitiesRequest extends GenericHTTPRequest<Charities[]> {
   constructor() {
-    const config: BaseAPIConfig = {
-      method: HttpMethod.GET,
-      contentType: ContentType.JSON,
-      url: URL,
-    };
+    const config = { ...charitiesConfig, method: HttpMethod.GET };
     super(config, isGetCharitiesResponse);
   }
 }
